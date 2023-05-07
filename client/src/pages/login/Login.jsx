e0fd4a3129b300831b8173263cd5e56f2fcef2c2
Bich Tram <bichtramlt1911@gmail.com>
2023-05-08 00:08:59 +0700

    login

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+import useFormLogin from "./useFormLogin";
+import validateUser from "./validateUser";
+const Login = ({ setIsAuticated }) => {
+  const [errorLogin, setErorLogin] = useState("");
+  const navigate = useNavigate()
+  const submitForm = () => {
+    const username = user.username;
+    const password = user.password;
+    axios
+      .post(
+        `http://localhost:8080/api/users/login?username=${username}&password=${password}`
+      )
+      .then((res) => {
+        localStorage.setItem("user", JSON.stringify(res.data));
+        //setIsAuticated(true);
+        navigate("/home");
+      })
+      .catch((e) => {
+        setErorLogin("Tên tài khoản hoặc mật khẩu không chính xác");
+      });
+    //Navigate to Screen
+  };
+  const { handleChange, handleSubmit, user, errors } = useFormLogin(
+    submitForm,
+    validateUser
+  );
 
-const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-container">
+      <div className="login-form">
+        <h3 id="login-form-title">ĐĂNG NHẬP</h3>
 
-export default Login
+        <div className="login-form-inputs">
+          <input
+            value={user.username}
+            className="login-form-input"
+            type="text"
+            placeholder="Tài khoản"
+            onChange={handleChange}
+            name="username"
+          />
+          <p className="login-form-error">{errors.username}</p>
+        </div>
+        <div className="login-form-inputs">
+          <input
+            className="login-form-input"
+            type="password"
+            placeholder="Mật khẩu"
+            value={user.password}
+            name="password"
+            onChange={handleChange}
+          />
+          <p className="login-form-error">{errors.password}</p>
+        </div>
+
+        <div className="login-form-row login-failed">
+          <p>{errorLogin}</p>
+        </div>
+        <div className="login-form-item">
+          <button onClick={handleSubmit} className="btn-login">
+            Đăng nhập
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
