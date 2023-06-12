3a30ec05fa15f4a49d46144f77a28454d159995a
Trường Nguyễn <102509826+truongnguyenuit@users.noreply.github.com>
2023-06-12 15:04:57 +0700

    update saleBill

--- server/models/SaleBill.js
+++ server/models/SaleBill.js
@@ -2,16 +2,38 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const SaleBillSchema = new Schema({
-  UserID: {
+  UserId: {
     type: mongoose.Types.ObjectId,
     ref: 'user',
   },
-  products: {
-    type: [{
-      type: mongoose.Types.ObjectId,
-      ref: 'product',
-    }],
-    default: ''
+  CustomerId: {
+    type: mongoose.Types.ObjectId,
+    ref: 'customer',
+  },
+  subTotal: {
+    type: Number,
+    default: 0,
+  },
+  discount: {
+    type: Number,
+    default: 0,
+  },
+  orderDetails:
+    [
+      {
+        product: {
+          type: mongoose.Types.ObjectId,
+          ref: 'product'
+        },
+        amount: {
+          type: Number,
+          default: 0
+        },
+      }
+    ],
+  updateAt: {
+    type: Date,
+    default: Date.now
   },
   createAt: {
     type: Date,
