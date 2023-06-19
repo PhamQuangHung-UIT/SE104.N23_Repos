bbabfae1603265afa1ccd4b67afbd219cd4a4275
Trường Nguyễn <102509826+truongnguyenuit@users.noreply.github.com>
2023-06-19 15:45:48 +0700

    remove img required

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -7,13 +7,13 @@ const Customer = require('../models/Customer')
 router.post('/', verifyToken, async (req, res) => {
   const { name, email, img, telephoneNumber, address } = req.body
 
-  if (!name || !email || !img || !telephoneNumber || !address)
+  if (!name || !email || !telephoneNumber || !address)
     return res.status(400).json({ success: false, message: 'Thiếu thông tin cần thiết' })
   try {
     const newCustomer = new Customer({
       name: name,
       email: email,
-      img: img,
+      img: img ? img : '',
       telephoneNumber: telephoneNumber,
       address: address
     })
